0dbd6398b454cbd2fbc68b2f7953f5bc979c7ed6
Antonio Candia <candia.antonio@gmail.com>
2019-03-08 16:06:48 -0500

    Cargado de datos y graficos

---
+++ data_wrangle.R
@@ -0,0 +1,42 @@
+# Automatically reformat the code (Ctrl+Shift+A in RStudio)
+# Import data
+library(readxl)
+
+cryptodata_tbl <- read_excel(
+        "cryptodata.xlsx",
+        sheet     = "values",
+        col_types = c(
+            "date",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric",
+            "numeric"
+        )
+    )
+
+library(tidyverse)
+library(tidyquant)
+
+cryptodata_tbl %>% 
+    glimpse() %>% 
+    summary()
+
+cryptodata_long_tblt <- cryptodata_tbl %>% 
+    gather(key = "key", value = "value", -date)
+
+
+prices_graph <- cryptodata_long_tblt %>% 
+    ggplot(aes(x = date, y = value)) +
+    geom_line() +
+    facet_wrap(~key, scales = "free") + 
+    theme_tq()    
---
+++ packages.R
@@ -0,0 +1,12 @@
+# Install Packages for Financial Analysis
+
+install.packages("tidyverse") 
+install.packages("lubridate") 
+install.packages("readxl") 
+install.packages("highcharter") 
+install.packages("tidyquant")
+install.packages("timetk")
+install.packages("tibbletime")
+install.packages("quantmod")
+install.packages("PerformanceAnalytics")
+install.packages("scales")
