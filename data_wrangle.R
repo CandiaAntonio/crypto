bf2f88cf7b6862a6cab600c6a3cade5f7f00a31b
CandiaAntonio <candia.antonio@gmail.com>
2019-03-09 13:27:58 -0500

    Calculo de Retornos Mensuales

--- data_wrangle.R
+++ data_wrangle.R
@@ -1,5 +1,5 @@
 # Automatically reformat the code (Ctrl+Shift+A in RStudio)
-# Import data
+# Import data----
 library(readxl)
 
 cryptodata_tbl <- read_excel(
@@ -26,17 +26,96 @@ cryptodata_tbl <- read_excel(
 
 library(tidyverse)
 library(tidyquant)
+library(forcats)
 
 cryptodata_tbl %>%
     glimpse() %>%
     summary()
 
+# Tidying to long data
+# Assets reordering as factors 
+# Factors ordered by risk
+
 cryptodata_long_tblt <- cryptodata_tbl %>%
-    gather(key = "key", value = "value",-date)
+    gather(key = "asset", value = "prices", -date) %>% 
+    mutate(asset = as_factor(asset)) %>% 
+    mutate(asset = fct_relevel(asset, levels = c(
+        "treasuries",
+        "ig",
+        "hy",
+        "em",
+        "commodities",
+        "sp500",
+        "crypto_index",
+        "bitcoin",
+        "ripple",
+        "ethereum",
+        "eos",
+        "litecoin",
+        "ethereum_classic",
+        "zcash"))) %>% 
+    mutate(asset_asfactor = as.numeric(asset)) %>% 
+    group_by(asset) %>% 
+    glimpse()
 
 
-prices_graph <- cryptodata_long_tblt %>%
-    ggplot(aes(x = date, y = value)) +
+# Visualizing prices graph
+
+daily_prices_graph <- cryptodata_long_tblt %>%
+    ggplot(aes(x = date, y = prices)) +
     geom_line() +
-    facet_wrap( ~ key, scales = "free") +
+    facet_wrap( ~ asset, scales = "free") +
     theme_tq()
+
+daily_prices_graph
+
+# Converting Daily Prices to daily returns in the tidyquant world
+
+returns_daily_long_tbl <- cryptodata_long_tblt %>% 
+    group_by(asset) %>% 
+    mutate(returns = (log(prices) - log(lag(prices))))
+
+# Visualizing returns graph
+
+returns_graph <- returns_daily_long_tbl %>% 
+    ggplot(aes(x = date, y = returns)) +
+    geom_line() +
+    facet_wrap( ~ asset, scales = "free") +
+    theme_tq()
+    
+returns_graph    
+
+# Convert data to tibbletime from daily to monthly
+
+library(tibbletime)
+
+data_monthly_tbl <- cryptodata_long_tblt %>% 
+    as_tbl_time(index = date) %>% 
+    as_period(period = "month", side = "end")
+
+# Visualizing monthly data
+
+monthly_prices_graph <- data_monthly_tbl %>%
+    ggplot(aes(x = date, y = prices)) +
+    geom_line() +
+    facet_wrap( ~ asset, scales = "free") +
+    theme_tq()
+
+monthly_prices_graph
+
+
+# Converting Daily Prices to daily returns in the tidyquant world
+    
+returns_monthly_long_tbl <- data_monthly_tbl %>% 
+    group_by(asset) %>% 
+    mutate(returns = (log(prices) - log(lag(prices))))
+
+# Visualizing monthly returns
+
+monthly_returns_graph <- returns_monthly_long_tbl %>%
+    ggplot(aes(x = date, y = returns)) +
+    geom_line() +
+    facet_wrap( ~ asset, scales = "free") +
+    theme_tq()
+
+monthly_returns_graph
